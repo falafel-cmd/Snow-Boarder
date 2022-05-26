b2fc938af9d02a61dd13bd8cf78f7c15a95d2f03
Fathi Mokhtar <70948032+falafel-cmd@users.noreply.github.com>
2022-05-26 19:53:05 +0100

    created new scripts
    CrashDetection will detect if player collider (head) is in contact with the floor and restart the level.
    
    GameFinish will end the game when the player gets to a certain area.

---
+++ SnowBoarder/Assets/_Scripts/CrashDetection.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Fathi Mokhtar
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetection : MonoBehaviour
+{
+
+	#region Variables
+	#endregion
+	
+	#region Unity Methods
+	
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+	
+	#endregion
+	
+}
