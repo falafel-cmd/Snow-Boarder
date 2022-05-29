1aae5aae44119526f2a3a1373f60484c8147a751
Fathi Mokhtar <70948032+falafel-cmd@users.noreply.github.com>
2022-05-29 14:47:47 +0100

    creation of particle effect when sliding

---
+++ SnowBoarder/Assets/_Scripts/ParticleTrail.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Fathi Mokhtar
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrail : MonoBehaviour
+{
+
+	#region Variables
+	[SerializeField] ParticleSystem trailFX;
+	#endregion
+
+	#region Unity Methods
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Ground")
+		{
+			trailFX.Pause();
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == "Ground")
+		{
+			trailFX.Play();
+		}
+	}
+	#endregion
+
+}
